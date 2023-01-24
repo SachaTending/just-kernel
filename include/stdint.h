06529343ba05c1ba3b7d350f97dc5c6c175a1ae2
SachaTending <65766775+SachaTending@users.noreply.github.com>
2023-01-24 21:07:42 +0300

    fix compilation issues on github actions

---
+++ include/stdint.h
@@ -0,0 +1,10 @@
+#ifndef _STDINT_H
+#define _STDINT_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long int uint64_t;
+
+
+#endif
